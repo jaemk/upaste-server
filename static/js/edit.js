ece87c8d4b4308f1a1d71075a0755cb4e9cd8d75
James Kominick <james.kominick@gmail.com>
2017-04-04 20:35:54 -0400

    edit.js | cursor control
    - add functions for getting and setting cursor position.
    - handle paste inputs at cursor position
    - handle tabs at cursor position
      - currently just replaces it with 4 spaces

--- static/js/edit.js
+++ static/js/edit.js
@@ -2,6 +2,92 @@
  * - Handles creating, editing, and viewing pastes.
  *
  */
+
+/// Getting and setting cursor position inside a contenteditable element:
+/// http://stackoverflow.com/a/41034697
+function createRange(node, chars, range) {
+    if (!range) {
+        range = document.createRange()
+        range.selectNode(node);
+        range.setStart(node, 0);
+    }
+    if (chars.count === 0) {
+        range.setEnd(node, chars.count);
+    } else if (node && chars.count >0) {
+        if (node.nodeType === Node.TEXT_NODE) {
+            if (node.textContent.length < chars.count) {
+                chars.count -= node.textContent.length;
+            } else {
+                range.setEnd(node, chars.count);
+                chars.count = 0;
+            }
+        } else {
+           for (var lp = 0; lp < node.childNodes.length; lp++) {
+                range = createRange(node.childNodes[lp], chars, range);
+
+                if (chars.count === 0) {
+                    break;
+                }
+            }
+        }
+    }
+    return range;
+};
+function setCurrentCursorPosition(editor, chars) {
+    if (chars >= 0) {
+        var selection = window.getSelection();
+
+        range = createRange(editor, { count: chars });
+
+        if (range) {
+            range.collapse(false);
+            selection.removeAllRanges();
+            selection.addRange(range);
+        }
+    }
+};
+function isChildOf(node, parentId) {
+    while (node !== null) {
+        if (node.id === parentId) {
+            return true;
+        }
+        node = node.parentNode;
+    }
+
+    return false;
+};
+function getCurrentCursorPosition(parentId) {
+    var selection = window.getSelection(),
+        charCount = -1,
+        node;
+
+    if (selection.focusNode) {
+        if (isChildOf(selection.focusNode, parentId)) {
+            node = selection.focusNode;
+            charCount = selection.focusOffset;
+
+            while (node) {
+                if (node.id === parentId) {
+                    break;
+                }
+
+                if (node.previousSibling) {
+                    node = node.previousSibling;
+                    charCount += node.textContent.length;
+                } else {
+                     node = node.parentNode;
+                     if (node === null) {
+                         break
+                     }
+                }
+            }
+        }
+    }
+    return charCount;
+};
+
+
+
 document.addEventListener("DOMContentLoaded", function() {
     var editor = document.getElementById("editor");         // editor container
     var save   = document.getElementById("save-paste");     // save-paste button/element
@@ -16,7 +102,7 @@ document.addEventListener("DOMContentLoaded", function() {
     /**
      * handleInput
      * - Handles changes to editor content from either an "input" `event` on the
-     *   editor or an explicit call providing an `_element`
+     *   editor or an explicit call providing an `_element` and updates the gutter line nums.
      *
      * @param event     - input event
      * @param _element  - optional target element. when specified, will use over event.target
@@ -50,7 +136,48 @@ document.addEventListener("DOMContentLoaded", function() {
     var editor = document.getElementById('editor');
     editor.addEventListener("input", handleInput, false);
 
-    /** Save
+
+    /** Handle clipboard pasting
+     *
+     */
+    editor.addEventListener("paste", function(e){
+        if (!editor.getAttribute('contenteditable')) { return; }
+        e.stopPropagation();
+        e.preventDefault();
+
+        var clipboardData = e.clipboardData || window.clipboardData;
+        var pastedData = clipboardData.getData('Text');
+
+        var pos = getCurrentCursorPosition('editor');
+        var head = editor.innerText.slice(0, pos);
+        var tail = editor.innerText.slice(pos);
+        editor.innerText = head + pastedData.replace(/ /g, '\u00a0') + tail;
+        setCurrentCursorPosition(editor, head.length + pastedData.length);
+        // update gutter
+        handleInput(null, editor);
+    });
+
+
+    /** Handle tabs
+     *
+     */
+    editor.addEventListener('keydown', function(e) {
+        if (e.keyCode !== 9 || !editor.getAttribute('contenteditable')) { return; }
+        e.stopPropagation();
+        e.preventDefault();
+        var tabWidth = parseInt(document.getElementById('tab-width'));
+        if (!tabWidth || isNaN(tabWidth)) { tabWidth = 4; }
+        var pos = getCurrentCursorPosition('editor');
+        var head = editor.innerText.slice(0, pos);
+        var tail = editor.innerText.slice(pos);
+        var tab = '\u00a0'.repeat(tabWidth);
+        editor.innerText = head + tab + tail;
+        setCurrentCursorPosition(editor, head.length + tab.length);
+        handleInput(null, editor);
+    });
+
+
+    /** Save content
      * - When the save button is present (which it should always be, might just be hidden),
      *   add a click listener to post current content and redirect to a viewable link
      */
@@ -59,7 +186,6 @@ document.addEventListener("DOMContentLoaded", function() {
             var editor = document.getElementById("editor");
             var content = editor.innerText;
 
-            console.log(content.split("\n"));
             var http = new XMLHttpRequest();
             var url  = "/new";
             http.open("POST", url, true);
@@ -75,12 +201,15 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     }
 
-    /** Edit
+
+    /** Edit existing content
      * - When the edit button is present, add a click listener to change up the header area.
      *   - hide the edit button
      *   - show the save button
      *   - change the paste-id to a helpful message
      *   - make editor area editable
+     *   - set the editor text to itself so it reformats from groups
+     *     of elements to only text
      */
     if (edit) {
         edit.addEventListener("click", function(){
@@ -89,10 +218,12 @@ document.addEventListener("DOMContentLoaded", function() {
             var key = document.getElementById("paste-id");
             key.innerText = "Hit 'Save' when you're finished!";
             editor.setAttribute('contenteditable', true);
+            editor.innerText = editor.innerText;
         });
     }
 
-    /** Load paste content
+
+    /** Initial load paste content
      * - When viewing a paste, the raw paste-content will be in a hidden div 'paste-text'.
      *   Copy this text into the editor and pass the editor to `handleInput` to update the gutter.`
      */
