263aefb300a2fd0e75092e8413d2cfe6d3408672
James Kominick <james.kominick@gmail.com>
2017-04-07 15:52:31 -0400

    add content_type to paste record

--- migrations/20170319223851_initial/up.sql
+++ migrations/20170319223851_initial/up.sql
@@ -2,6 +2,7 @@ create table pastes (
     id            serial PRIMARY KEY,
     key           text UNIQUE NOT NULL,
     content       text NOT NULL,
+    content_type  text NOT NULL DEFAULT 'text',
     date_created  timestamp WITH TIME ZONE NOT NULL DEFAULT NOW(),
     date_viewed   timestamp WITH TIME ZONE NOT NULL DEFAULT NOW()
 );
--- src/models.rs
+++ src/models.rs
@@ -2,19 +2,61 @@ use schema::pastes;
 use chrono::{DateTime, UTC};
 
 
-#[derive(Queryable, Debug)]
-pub struct Paste {
-    pub id: i32,
-    pub key: String,
-    pub content: String,
-    pub date_created: DateTime<UTC>,
-    pub date_viewed: DateTime<UTC>,
-}
-
-
 #[derive(Insertable)]
 #[table_name="pastes"]
 pub struct NewPaste<'a> {
     pub key: String,
     pub content: &'a str,
 }
+
+
+#[derive(Queryable, Debug)]
+pub struct Paste {
+    pub id: i32,
+    pub key: String,
+    pub content: String,
+    pub content_type: String,
+    pub date_created: DateTime<UTC>,
+    pub date_viewed: DateTime<UTC>,
+}
+impl Paste {
+    pub fn content_types() -> Vec<String> {
+        [
+            "text",
+            "apache",
+            "css",
+            "http",
+            "javascript",
+            "ruby",
+            "bash",
+            "ini",
+            "diff",
+            "json",
+            "sql",
+            "markdown",
+            "perl",
+            "c++",
+            "html",  // and xml?
+            "java",
+            "nginx",
+            "python",
+            "clojure",
+            "ocaml",
+            "scala",
+            "vim",  // or vim-script?
+            "go",
+            "haskell",
+            "swift",
+            "elixer",
+            "django",
+            "dockerfile",
+            "elm",
+            "lisp",
+            "rust",
+            "yaml",
+        ].iter()
+         .map(|s| s.to_string())
+         .collect::<Vec<_>>()
+    }
+}
+
