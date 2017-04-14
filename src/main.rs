97efd5dbd8a8e726d89bc952c1c49072159dd662
James Kominick <james.kominick@gmail.com>
2017-04-13 20:28:26 -0400

    serve --args
    - default to localhost with flag '--public' to serve on '0.0.0.0'

--- src/main.rs
+++ src/main.rs
@@ -23,9 +23,9 @@ pub fn main() {
                          .short("p")
                          .takes_value(true)
                          .help("Port to listen on. Defaults to 3000"))
-                    .arg(Arg::with_name("private")
-                         .long("private")
-                         .help("Serve on 'localhost' instead of '0.0.0.0'"))
+                    .arg(Arg::with_name("public")
+                         .long("public")
+                         .help("Serve on '0.0.0.0' instead of 'localhost'"))
                     .arg(Arg::with_name("silent")
                          .long("silent")
                          .help("Don't output any logging info")))
@@ -68,7 +68,7 @@ pub fn main() {
 fn run(matches: ArgMatches) -> Result<()> {
     if let Some(serve_matches) = matches.subcommand_matches("serve") {
         let port = serve_matches.value_of("port").unwrap_or("3000");
-        let host_base = if serve_matches.is_present("private") { "localhost" } else { "0.0.0.0" };
+        let host_base = if serve_matches.is_present("public") { "0.0.0.0" } else { "localhost" };
         let host = format!("{}:{}", host_base, port);
         let do_log = !serve_matches.is_present("silent");
         let db_url = serve_matches.value_of("database");
