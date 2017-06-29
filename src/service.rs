5a4d7b6c9b9803c6866920544bff4c85c9bcc0fb
James Kominick <james.kominick@gmail.com>
2017-06-28 23:55:24 -0400

    favicon/robots
    - add robots.txt
    - add fallback handlers

--- src/service.rs
+++ src/service.rs
@@ -145,6 +145,8 @@ pub fn start(host: &str, db: Option<&str>) {
     // mount our chain of services and a static file handler
     let mut mount = Mount::new();
     mount.mount("/", chain)
+         .mount("/favicon.ico", Static::new(Path::new("static/favicon.ico")))
+         .mount("/robots.txt", Static::new(Path::new("static/robots.txt")))
          .mount("/static/", Static::new(Path::new("static")));
 
     info!(" ** Serving at {} **", host);
