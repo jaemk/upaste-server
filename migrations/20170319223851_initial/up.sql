bddfb00af253ac426ad5b82470ce8ffbe820a8ec
James Kominick <james.kominick@gmail.com>
2017-11-04 22:33:38 -0400

    modify migrations for sqlite

--- migrations/20170319223851_initial/up.sql
+++ migrations/20170319223851_initial/up.sql
@@ -1,9 +1,9 @@
 create table pastes (
-    id            serial PRIMARY KEY,
-    key           text UNIQUE NOT NULL,
-    content       text NOT NULL,
-    content_type  text NOT NULL DEFAULT 'text',
-    date_created  timestamp WITH TIME ZONE NOT NULL DEFAULT NOW(),
-    date_viewed   timestamp WITH TIME ZONE NOT NULL DEFAULT NOW()
+    id              integer PRIMARY KEY AUTOINCREMENT,
+    key             text UNIQUE NOT NULL,
+    content         text NOT NULL,
+    content_type    text NOT NULL DEFAULT 'text',
+    date_created    unsigned big int NOT NULL DEFAULT (cast(strftime('%s', 'now') as unsigned big int)),
+    date_viewed     unsigned big int NOT NULL DEFAULT (cast(strftime('%s', 'now') as unsigned big int))
 );
 
